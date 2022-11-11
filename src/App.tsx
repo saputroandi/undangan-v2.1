40b80ceac99c26c31bb837c8985d2c6212536552
saputroandi <57069797+saputroandi@users.noreply.github.com>
2022-11-11 13:03:11 +0700

    add bank and music modal, also change navigation size
    add bank and music modal, also change navigation size

--- src/App.tsx
+++ src/App.tsx
@@ -7,49 +7,56 @@ import CouplePhotoBottom from "./components/CouplePhotoBottom";
 import Event from "./components/Event";
 import Gallery from "./components/Gallery";
 import Maps from "./components/Maps";
-import Modal from "./components/Modal";
+import WelcomeModal from "./components/WelcomeModal";
 import Navigation from "./components/Navigation";
 import Opening from "./components/Opening";
 import Protocol from "./components/Protocol";
 import Wishes from "./components/Wishes";
+import BankModal from "./components/BankModal";
+import MusicModal from "./components/MusicModal";
 
 type AppProps = {
-  children?: React.ReactNode;
+	children?: React.ReactNode;
 };
 
 const App: React.FC = (props: AppProps): React.ReactElement => {
-  const [showModal, setShowModal] = useState<boolean>(true);
-  const components: React.ReactElement[] = [
-    <CouplePhoto />,
-    <Countdown />,
-    <Opening />,
-    <Couple />,
-    <Event />,
-    <Maps />,
-    <Gallery />,
-    <Wishes />,
-    <Protocol />,
-    <CouplePhotoBottom />,
-  ];
+	const [showModal, setShowModal] = useState<boolean>(true);
+	const components: React.ReactElement[] = [
+		<BankModal />,
+		<MusicModal />,
+		<CouplePhoto />,
+		<Countdown />,
+		<Opening />,
+		<Couple />,
+		<Event />,
+		<Maps />,
+		<Gallery />,
+		<Wishes />,
+		<Protocol />,
+		<CouplePhotoBottom />,
+	];
 
-  return (
-    <>
-      {components.map((component) => {
-        return (
-          <main>
-            {showModal ? (
-              <Modal showModal={showModal} setShowModal={setShowModal} />
-            ) : (
-              <>
-                <BaseComponent>{component}</BaseComponent>
-                <Navigation />
-              </>
-            )}
-          </main>
-        );
-      })}
-    </>
-  );
+	return (
+		<>
+			{components.map((component) => {
+				return (
+					<main>
+						{showModal ? (
+							<WelcomeModal
+								showWelcomeModal={showModal}
+								setShowWelcomeModal={setShowModal}
+							/>
+						) : (
+							<>
+								<BaseComponent>{component}</BaseComponent>
+								<Navigation />
+							</>
+						)}
+					</main>
+				);
+			})}
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/BankModal.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SlideComp, { Direction } from "./animate/SlideComp";
+
+type BankModalProps = {
+	children?: React.ReactElement;
+};
+
+const BankModal: React.FC<BankModalProps> = (props) => {
+	return (
+		<>
+			<div className="fixed top-0 left-0 w-12 h-12 z-10">
+				<SlideComp>
+					<div className="w-full h-full rounded-full bg-gray-700">
+						<svg
+							className="w-full h-full p-2 fill-white stroke-white"
+							viewBox="0 0 256 256"
+						>
+							<path d="M216 76h-46.2a25.4 25.4 0 0 0 6.3-4.5a28 28 0 1 0-39.6-39.6c-3.6 3.6-6.4 9.2-8.5 16.7c-2.1-7.5-4.9-13.1-8.5-16.7a28 28 0 0 0-39.6 39.6a25.4 25.4 0 0 0 6.3 4.5H40a12 12 0 0 0-12 12v32a12 12 0 0 0 12 12h4v68a12 12 0 0 0 12 12h144a12 12 0 0 0 12-12v-68h4a12 12 0 0 0 12-12V88a12 12 0 0 0-12-12Zm-73.9-38.4a20.2 20.2 0 0 1 14.2-5.9a20.1 20.1 0 0 1 14.1 34.2c-7.2 7.2-27.3 9.5-38.3 10c.5-11 2.8-31.1 10-38.3Zm-56.5 0a20 20 0 0 1 28.3 0c7.2 7.2 9.5 27.3 10 38.3c-11-.5-31.1-2.8-38.3-10a20 20 0 0 1 0-28.3ZM36 120V88a4 4 0 0 1 4-4h84v40H40a4 4 0 0 1-4-4Zm16 80v-68h72v72H56a4 4 0 0 1-4-4Zm152 0a4 4 0 0 1-4 4h-68v-72h72Zm16-80a4 4 0 0 1-4 4h-84V84h84a4 4 0 0 1 4 4Z"></path>
+						</svg>
+					</div>
+				</SlideComp>
+			</div>
+		</>
+	);
+};
+
+export default BankModal;
